d740ab0195930842ffc152f92c4c1e0a93b6c8d1
Jeongbin <yon980824@gmail.com>
2022-02-09 11:36:16 +0900

    JS: CheckMenu - select date, load json

--- src/checkMenu.js
+++ src/checkMenu.js
@@ -1,44 +1,73 @@
 let sumCal = 0;
 let url =
-  "http://openapi.foodsafetykorea.go.kr/api/92804346bfe547c5a581/COOKRCP01/json/1/5/RCP_NM=";
-let when = "menus[0].breakfast";
+  "http://openapi.foodsafetykorea.go.kr/api/92804346bfe547c5a581/COOKRCP01/json/1/5/";
+
+onLoadFood(url);
+
+const today = new Date();
+const currentYear = `${today.getFullYear()}`;
+const currentMonth = `${today.getMonth() + 1}`;
+const currentDate = `${today.getDate()}`;
+let order = currentDate - 1;
+const todayDate = currentYear + currentMonth + currentDate;
+loadSelectedDate(todayDate, order);
+
+// Click select button
+const dateSelectBtn = document.querySelector(".calendar .calendar__select");
+dateSelectBtn.addEventListener("click", () => {
+  setTimeout(() => {
+    let selected = JSON.parse(sessionStorage.getItem("selectedDay"));
+    let selectedDate =
+      `${selected.selectedYear}` +
+      `${selected.selectedMonth}` +
+      `${selected.selectedDate}`;
+    order = `${selected.selectedDate - 1}`;
+    loadSelectedDate(selectedDate, order);
+  }, 500);
+});
 
 // Load menus
-loadMenu().then((menus) => {
-  //   onLoadFood(foodURL("두루치기"));
-  const content = document.querySelector(`#checkMenu .menus`);
-  menus[2].dinner.map((menu) => {
-    const li = addMenu(menu);
-    content.append(li);
+function loadSelectedDate(selectedDate, order) {
+  loadMenu().then((menus) => {
+    const content = document.querySelector(`#checkMenu .menus`);
+    content.innerHTML = "";
+    console.log(menus[order][`${selectedDate}`]);
+    menus[order][`${selectedDate}`] &&
+      menus[order][`${selectedDate}`][0].breakfast.map((menu) => {
+        // onLoadFood(foodURL(menu));
+        console.log(menu);
+        const li = addMenu(menu);
+        content.append(li);
+      });
+
+    const navBtn = document.querySelectorAll("#nav div");
+    for (const btn of navBtn) {
+      btn.addEventListener("click", () => {
+        const siblings = [...navBtn].filter((e) => e !== btn);
+        content.innerHTML = "";
+        btn.classList.add("active");
+        siblings.map((el) => el.classList.remove("active"));
+
+        if (btn.classList.contains("nav__breakfast")) {
+          menus[order][`${selectedDate}`][0].breakfast.map((menu) => {
+            const li = addMenu(menu);
+            content.append(li);
+          });
+        } else if (btn.classList.contains("nav__lunch")) {
+          menus[order][`${selectedDate}`][1].lunch.map((menu) => {
+            const li = addMenu(menu);
+            content.append(li);
+          });
+        } else if (btn.classList.contains("nav__dinner")) {
+          menus[order][`${selectedDate}`][2].dinner.map((menu) => {
+            const li = addMenu(menu);
+            content.append(li);
+          });
+        }
+      });
+    }
   });
-
-  const navBtn = document.querySelectorAll("#nav div");
-  for (const btn of navBtn) {
-    btn.addEventListener("click", () => {
-      const siblings = [...navBtn].filter((e) => e !== btn);
-      content.innerHTML = "";
-      btn.classList.add("active");
-      siblings.map((el) => el.classList.remove("active"));
-
-      if (btn.classList.contains("nav__breakfast")) {
-        menus[0].breakfast.map((menu) => {
-          const li = addMenu(menu);
-          content.append(li);
-        });
-      } else if (btn.classList.contains("nav__lunch")) {
-        menus[1].lunch.map((menu) => {
-          const li = addMenu(menu);
-          content.append(li);
-        });
-      } else if (btn.classList.contains("nav__dinner")) {
-        menus[2].dinner.map((menu) => {
-          const li = addMenu(menu);
-          content.append(li);
-        });
-      }
-    });
-  }
-});
+}
 
 // Load food's Ingredients data
 function onLoadFood(url) {
